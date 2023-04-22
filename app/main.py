1098482fe730696da2a87ebcfbee1ab072c99517
Maxim Vasilkin <mxvas@mail.ru>
2023-04-22 14:14:10 +0400

    add error handlers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 import json
-
-from aiohttp import web, hdrs
-import asyncio
+from aiohttp import web
 from pydantic import ValidationError
 from app_errors import HttpError, IntegrityError
 from response_statuses import Status
@@ -10,7 +8,6 @@ from validators import PostAdv, PatchAdv, PostUser, PatchUser
 from work_with_db import db, create_tables, dispose
 from authenticate import get_hashed_password, check_password
 
-from bcrypt import hashpw, gensalt, checkpw
 
 
 # settings
@@ -45,22 +42,16 @@ def validate(json, validate_model_class):
         model = validate_model_class(**json)
         validated_json = model.dict(exclude_none=True)
         if not validated_json:
-            raise HttpError(400, 'Validation error')
+            raise web.HTTPBadRequest(text=json.dumps({'error': 'Validation error'}), content_type='application/json')
         return validated_json
     except ValidationError as error:
-        raise HttpError(400, error.errors())
-
-
-def __error_message(message, status_code):
-    response = web.json_response({'message': message} | Status.error)
-    response.status_code = status_code
-    return response
+        raise web.HTTPBadRequest(text=json.dumps({'error': error.errors()}), content_type='application/json')
 
 
 async def get_object_and_check(model, attr, value, to_dict=True):
     object = await db.get_object_by_attr(model, attr, value, to_dict=to_dict)
     if object is None:
-        raise HttpError(404, f'{model.__name__} not found')
+        raise  web.HTTPNotFound(text=json.dumps({'error': f'{model.__name__} not found'}), content_type='application/json')
     return object
 
 
@@ -68,10 +59,10 @@ async def authenticate(request: web.Request):
     email = request.headers.get('email')
     password = request.headers.get('password')
     if not email or not password:
-        raise HttpError(410, 'Empty email or password')
+        raise web.HTTPBadRequest(text=json.dumps({'error': 'Empty email or password'}), content_type='application/json')
     user = await db.get_object_by_attr(User, 'email', email)
     if not user or not check_password(password, user.password):
-        raise HttpError(410, 'Invalid authenticate')
+        raise web.HTTPNotAcceptable(text=json.dumps({'error': 'Invalid authenticate'}), content_type='application/json')
     return user
 
 
@@ -93,7 +84,13 @@ async def get_adv_checked_request(request: web.Request, validate_model):
 
 async def check_adv_owner(user_id, adv_id):
     if not await db.check_rights_on_adv(user_id, adv_id):
-        raise HttpError(404, 'Can not manipulate with this advertisment')
+        raise web.HTTPForbidden(text=json.dumps({'error': 'Can not manipulate with this advertisment'}),
+                                content_type='application/json')
+
+
+def _non_ascii_response(json_, **kwargs):
+    response = web.Response(text=json.dumps(json_, ensure_ascii=False), content_type='application/json', **kwargs)
+    return response
 
 
 # views
@@ -102,13 +99,16 @@ class UserView(web.View):
     async def get(self):
         user_id = int(self.request.match_info['user_id'])
         user = await get_object_and_check(User, 'id', user_id)
-        return web.json_response(user | Status.ok)
+        return _non_ascii_response(user | Status.ok)
 
     async def post(self):
         validated_json = await get_user_checked_request(self.request, PostUser)
-        await db.create_object(User, **validated_json)
+        try:
+            await db.create_object(User, **validated_json)
+        except IntegrityError:
+            raise web.HTTPConflict(text=json.dumps({'error': 'Existance error'}), content_type='application/json')
         del validated_json['password']
-        return web.json_response(validated_json | Status.ok)
+        return _non_ascii_response(validated_json | Status.ok)
 
     async def patch(self):
         validated_json = await get_user_checked_request(self.request, PatchUser)
@@ -117,33 +117,36 @@ class UserView(web.View):
         await db.update_object(User, user_id, **validated_json)
         if 'password' in validated_json:
             del validated_json['password']
-        return web.json_response(validated_json | Status.ok)
+        return _non_ascii_response(validated_json | Status.ok)
 
     async def delete(self):
         user_id = int(self.request.match_info['user_id'])
         user = await get_object_and_check(User, 'id', user_id)
         await db.delete_object(User, user_id)
-        return web.json_response(user | Status.ok)
+        return _non_ascii_response(user | Status.ok)
 
 
 class AdvView(web.View):
     async def get(self):
         adv_id = int(self.request.match_info['adv_id'])
         adv = await get_object_and_check(Advertisment, 'id', adv_id)
-        return web.json_response(adv | Status.ok)
+        return _non_ascii_response(adv | Status.ok)
 
     async def post(self):
         user, validated_json = await get_adv_checked_request(self.request, PostAdv)
         validated_json['owner_id'] = user.id
-        await db.create_object(Advertisment, **validated_json)
-        return web.Response(text=json.dumps(validated_json, ensure_ascii=False), content_type='application/json')
+        try:
+            await db.create_object(Advertisment, **validated_json)
+        except IntegrityError:
+            raise web.HTTPConflict(text=json.dumps({'error': 'Existance error'}), content_type='application/json')
+        return _non_ascii_response(validated_json | Status.ok)
 
     async def patch(self):
         adv_id = int(self.request.match_info['adv_id'])
         user, validated_json = await get_adv_checked_request(self.request, PatchAdv)
         await check_adv_owner(user.id, adv_id)
         await db.update_object(Advertisment, adv_id, **validated_json)
-        return web.json_response(validated_json | Status.ok)
+        return _non_ascii_response(validated_json | Status.ok)
 
     async def delete(self):
         adv_id = int(self.request.match_info['adv_id'])
@@ -151,7 +154,7 @@ class AdvView(web.View):
         await check_adv_owner(user.id, adv_id)
         await db.delete_object(Advertisment, adv_id)
         message = {'advertisment': adv_id}
-        return web.json_response(message | Status.ok)
+        return _non_ascii_response(message | Status.ok)
 
 
 # urls
